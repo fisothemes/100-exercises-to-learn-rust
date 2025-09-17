766559abc60f0dc05ea13206620eac16371da602
Goodwill Mzumala <fisothemes@gmail.com>
2025-09-17 13:26:55 +0100

    completed (02) vectors for 6.Ticket Management

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -10,12 +10,19 @@
 //
 // We expect `fibonacci(0)` to return `0`, `fibonacci(1)` to return `1`,
 // `fibonacci(2)` to return `1`, and so on.
+
 pub fn fibonacci(n: u32) -> u32 {
     // TODO: implement the `fibonacci` function
     //
     // Hint: use a `Vec` to memoize the results you have already calculated
     // so that you don't have to recalculate them several times.
-    todo!()
+    let mut memo = vec![0, 1];
+    let n: usize = n as usize;
+
+    for i in 0..n {
+        memo.push(memo[i + 1] + memo[i]);
+    }
+    memo[n]
 }
 
 #[cfg(test)]
