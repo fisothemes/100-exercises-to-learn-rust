d12ddb95e3ccb53ccc573b3b69d73118165579c7
Goodwill Mzumala <fisothemes@gmail.com>
2025-09-19 23:18:43 +0100

    completed (01) threads for 7.Threads

--- exercises/07_threads/01_threads/src/lib.rs
+++ exercises/07_threads/01_threads/src/lib.rs
@@ -14,8 +14,19 @@
 // this is necessary in the next exercise.
 use std::thread;
 
-pub fn sum(v: Vec<i32>) -> i32 {
-    todo!()
+pub fn sum<'a>(v: Vec<i32>) -> i32 {
+    let mut v1 = v;
+    let v2 = v1.split_off(v1.len() / 2);
+
+
+    let h1 = thread::spawn(move || {
+        v1.iter().sum::<i32>()
+    });
+    let h2 = thread::spawn(move || {
+        v2.iter().sum::<i32>()
+    });
+
+    h1.join().unwrap() + h2.join().unwrap()
 }
 
 #[cfg(test)]
