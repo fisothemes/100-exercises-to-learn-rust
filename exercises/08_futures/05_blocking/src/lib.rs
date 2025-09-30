9b42f92e320b34dff4b3070ba40806eacfe2d731
Goodwill Mzumala <fisothemes@gmail.com>
2025-09-30 03:41:24 +0100

    completed (05) blocking for 8.Futures

--- exercises/08_futures/05_blocking/src/lib.rs
+++ exercises/08_futures/05_blocking/src/lib.rs
@@ -2,17 +2,24 @@
 //  When running the tests, you should observe that it hangs, due to a
 //  deadlock between the caller and the server.
 //  Use `spawn_blocking` inside `echo` to resolve the issue.
+
 use std::io::{Read, Write};
 use tokio::net::TcpListener;
 
 pub async fn echo(listener: TcpListener) -> Result<(), anyhow::Error> {
+
     loop {
         let (socket, _) = listener.accept().await?;
         let mut socket = socket.into_std()?;
-        socket.set_nonblocking(false)?;
-        let mut buffer = Vec::new();
-        socket.read_to_end(&mut buffer)?;
-        socket.write_all(&buffer)?;
+
+        let h = tokio::task::spawn_blocking(move || -> Result<(), anyhow::Error> {
+            socket.set_nonblocking(false)?;
+            let mut buffer = Vec::new();
+            socket.read_to_end(&mut buffer)?;
+            Ok(socket.write_all(&buffer)?)
+        });
+
+        h.await??
     }
 }
 
